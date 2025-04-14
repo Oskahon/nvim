bce35d307deb302ba8ea830d8aa93db8ffe13fca
Oskari Ahonen <ahonenoskari@gmail.com>
2025-04-14 16:52:01 +0300

    Add some keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,13 @@
 ------------
 -- Keymaps
 ------------
+
+-- Save file
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+-- Close file
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Close file' })
+
+-- Close buffer
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Close buffer' })
+-- Close other buffers
+vim.keymap.set('n', '<leader>bo', ':%bd|e#|bd#<CR>', { desc = 'Close other buffers' })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,15 +3,20 @@
 ------------
 
 -- Set the leader key
-vim.g.mapleader = "<Space>"
-
--- Tabs
+vim.g.mapleader = " "
 
 -- Set amount to spaces tab creates
 vim.opt.softtabstop = 4
+
 -- Set amount of spaces used for indentation (e.g. >> or <<)
 vim.opt.shiftwidth = 4
+
 -- Use spaces instead of tabs
 vim.opt.expandtab = true
 
-vim.opt.wrap = false
+-- Set word wrap
+vim.opt.wrap = true
+
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })

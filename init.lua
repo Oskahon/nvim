c67605f92702ad56d6d53df0562a434542852047
Oskari Ahonen <ahonenoskari@gmail.com>
2025-04-14 15:12:23 +0300

    Tab settings

--- init.lua
+++ init.lua
@@ -1,8 +1,18 @@
--- Let's start here
+------------
+-- Options
+------------
+
+-- Tabs
+
+-- Set amount to spaces tab creates
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
 
 -- Transparent background
 vim.cmd([[
-	highlight Normal guibg=none ctermbg=none
-	highlight NormalNC guibg=none ctermbg=none
-	highlight NonText guibg=none ctermbg=none
+    highlight Normal guibg=none ctermbg=none
+    highlight NormalNC guibg=none ctermbg=none
+    highlight NonText guibg=none ctermbg=none
 ]])

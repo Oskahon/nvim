dd7b2fcb1ed42b5a8ba92f9ab0294082601fea01
Oskari Ahonen <ahonenoskari@gmail.com>
2025-04-14 15:09:03 +0300

    Initial init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+-- Let's start here
+
+-- Transparent background
+vim.cmd([[
+	highlight Normal guibg=none ctermbg=none
+	highlight NormalNC guibg=none ctermbg=none
+	highlight NonText guibg=none ctermbg=none
+]])

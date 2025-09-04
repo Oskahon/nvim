e88556e2369a2a7891a6b5722ae5d0c6118741da
Oskahon <ahonenoskari@gmail.com>
2025-09-04 23:18:19 +0300

    Add keymaps for Gzz and moving lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,9 +3,9 @@
 ------------
 
 -- Save file
-vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
--- Close file
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Close file' })
+-- Close file
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 
 -- Open Ex Browser
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', { silent = true, desc = 'Open Ex' })
@@ -24,6 +24,7 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', 'G', 'Gzz')
 
 -- Navigate quickfix list
 vim.keymap.set('n', '[c', ':cnext<CR>zz', { silent = true })
@@ -52,3 +53,15 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 
 -- Keymap for search and replace in selected block
 vim.keymap.set('x', '<leader>s', ':s/\\%V')
+
+-- Move line up and down
+vim.keymap.set('n', '<M-j>', ':m .+1<CR>==', { silent = true })
+vim.keymap.set('n', '<M-k>', ':m .-2<CR>==', { silent = true })
+
+-- Move selected lines up and down
+vim.keymap.set('v', '<M-j>', ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set('v', '<M-k>', ":m '<-2<CR>gv=gv", { silent = true })
+
+-- Move line up and down in insert mode
+vim.keymap.set('i', '<M-j>', '<Esc>:m .+1<CR>==gi', { silent = true })
+vim.keymap.set('i', '<M-k>', '<Esc>:m .-2<CR>==gi', { silent = true })

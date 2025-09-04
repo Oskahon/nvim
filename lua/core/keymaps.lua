a0749cf328a1de528d311546d1c67e722f780649
Oskari Ahonen <ahonenoskari@gmail.com>
2025-09-04 12:49:08 +0300

    search and replace in selection

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -49,3 +49,6 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+
+-- Keymap for search and replace in selected block
+vim.keymap.set('x', '<leader>s', ':s/\\%V')
